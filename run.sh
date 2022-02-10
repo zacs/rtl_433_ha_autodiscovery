914924af6c26472b4a73c141a99d62059a072e97
Zac Schellhardt <zac@z12t.com>
2022-02-10 14:44:28 -0800

    sh eyeroll

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 echo "Starting rtl_433_mqtt_hass.py..."
 python3 -u /rtl_433_mqtt_hass.py -d -u ${MQTT_USERNAME} -P ${MQTT_PASSWORD} -H ${MQTT_HOST} -D ${DISCOVERY_PREFIX} -R ${RTL_TOPIC}
